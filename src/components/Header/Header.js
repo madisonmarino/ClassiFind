3db3d13f5cc0091a084dda308744e1888a4bcd2a
Madison Marino <marinomadison@gmail.com>
2024-01-28 17:27:33 -0500

    create and style header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header__content">
+        <h1 className="header__title">ClassiFind</h1>
+        <div className="header__links">
+          <NavLink to="/" className="header__link">
+            Home
+          </NavLink>
+          <NavLink to="recommendations" className="header__link">
+            Quiz
+          </NavLink>
+          <NavLink to="mymusic" className="header__link">
+            My Music
+          </NavLink>
+          <NavLink to="login" className="header__link">
+            Login
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
+}
